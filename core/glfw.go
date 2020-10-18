17fb57db14332a957ac4aae83323673ee8b5e8d3
Amar Bajric <abajric@pm.me>
2020-10-18 17:35:20 +0200

    feat(core): Add glfw for window init and input handling

---
+++ core/glfw.go
@@ -0,0 +1,25 @@
+package core
+
+import "github.com/go-gl/glfw/v3.3/glfw"
+
+// initGlfw initializes glfw and returns a Window to use.
+func InitGlfw() *glfw.Window {
+	if err := glfw.Init(); err != nil {
+		panic(err)
+	}
+	glfw.WindowHint(glfw.Resizable, glfw.False)
+	glfw.WindowHint(glfw.ContextVersionMajor, 4)
+	glfw.WindowHint(glfw.ContextVersionMinor, 1)
+	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
+	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
+
+	window, err := glfw.CreateWindow(width, height, title, nil, nil)
+	if err != nil {
+		panic(err)
+	}
+	// bind window to current thread
+	window.MakeContextCurrent()
+
+	return window
+}
+
