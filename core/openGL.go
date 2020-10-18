52b4e7b5805d501d9acb57c15f494c8dbd788537
Amar Bajric <abajric@pm.me>
2020-10-18 17:34:32 +0200

    feat(core): Add opengl and shader functionality for initializing program and shaders

---
+++ core/openGL.go
@@ -0,0 +1,31 @@
+package core
+
+import (
+	"github.com/go-gl/gl/v4.1-core/gl"
+	"log"
+)
+
+// initializes OpenGL and returns an initialized program
+func InitOpenGL() uint32 {
+	if err := gl.Init(); err != nil {
+		panic(err)
+	}
+	version := gl.GoStr(gl.GetString(gl.VERSION))
+	log.Println("OpenGL version", version)
+
+	vertexShader, err := CompileShader(VertexShaderSource, gl.VERTEX_SHADER)
+	if err != nil {
+		panic(err)
+	}
+
+	fragmentShader, err := CompileShader(FragmentShaderSource, gl.FRAGMENT_SHADER)
+	if err != nil {
+		panic(err)
+	}
+
+	prog := gl.CreateProgram()
+	gl.AttachShader(prog, vertexShader)
+	gl.AttachShader(prog, fragmentShader)
+	gl.LinkProgram(prog)
+	return prog
+}
