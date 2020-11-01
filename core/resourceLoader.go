f067abe443ce9a69ac8ba5f8a5ecfc3c444d2eab
Amar Bajric <abajric@pm.me>
2020-11-01 03:30:17 +0100

    feat(core): Implement resource loader for loading external resource like shader programs

---
+++ core/resourceLoader.go
@@ -0,0 +1,41 @@
+package core
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+const ShaderResources = "./res/shaders/"
+
+func loadShader(fileName string) string {
+	shaderProgram := fmt.Sprint()
+	shaderFile, err := os.Open(ShaderResources + fileName)
+	if err != nil {
+		log.Fatal(fmt.Sprintf("Error while %s shader '%s': ", "loading", fileName), err)
+	}
+
+	defer func() {
+		if err = shaderFile.Close(); err != nil {
+			log.Fatal(fmt.Sprintf("Error while %s shader file '%s': ", "closing", fileName), err)
+		}
+	}()
+
+	bufferReader := bufio.NewReader(shaderFile)
+	buffer := make([]byte, 1024)
+
+	for {
+		chunkLength, err := bufferReader.Read(buffer)
+		if err != nil {
+			if err.Error() != "EOF" {
+				log.Fatal(fmt.Sprintf("Error while %s shader file '%s': ", "reading", fileName), err)
+			} else {
+				shaderProgram += "\x00"
+				break
+			}
+		}
+		shaderProgram += string(buffer[0:chunkLength])
+	}
+	return shaderProgram
+}
