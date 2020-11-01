5ae1ffef92e81c62e61ab7188bd335ab0ee2d852
Amar Bajric <abajric@pm.me>
2020-11-01 11:17:41 +0100

    feat(glsl): Add color interpolation based on pixel position in fragment shader

--- res/shaders/fragmentShader.glsl
+++ res/shaders/fragmentShader.glsl
@@ -1,5 +1,6 @@
 #version 410
+in vec4 color;
 out vec4 frag_colour;
 void main() {
-    frag_colour = vec4(1, 1, 1, 1.0);
+    frag_colour = color;
 }
--- res/shaders/vertexShader.glsl
+++ res/shaders/vertexShader.glsl
@@ -1,5 +1,7 @@
 #version 410
 in vec3 vp;
+out vec4 color;
 void main() {
-    gl_Position = vec4(0.25 * vp, 1.0);
+    color = vec4(clamp(vp, 0.0, 1.0), 1.0);
+    gl_Position = vec4(vp, 1.0);
 }
